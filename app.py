759703bf50a52877433660338f464cb71a54ffa6
Sousa <Vinicius.dali2022@gmail.com>
2023-01-11 09:24:15 -0300

    add2

---
+++ app.py
@@ -0,0 +1,38 @@
+import mysql.connector 
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+conexao = mysql.connector.connect(
+host='localhost',
+user='root',
+password='vini12345',
+database='cadastro'
+)
+
+cursor = conexao.cursor(buffered=True)
+
+comando='SELECT * FROM usuarios'
+cursor.execute(comando)
+conexao.commit()
+resultado = cursor.fetchall()
+print(resultado)
+
+cursor.close()
+conexao.close()
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+@app.route('/send' , methods=['POST'])
+def send():
+    comando = '''
+    insert into cad_usuarios (nome,email,sexo,nacionalidade)
+    values('vini', 'vini1234.gmail.com', 'M', 'br')'''
+    cursor.execute(comando)
+    cursor.commit()
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
---
+++ app2.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, render_template, request
+import requests
+import mysql.connector 
+
+conexao = mysql.connector.connect(
+host='localhost',
+user='root',
+password='vini12345',
+database='cadastro'
+) 
+
+cursor = conexao.cursor(buffered=True)
+
+app = Flask(__name__)
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    email = request.form.get('email')
+    sexo = request.form.get('sexo')
+    pais = request.form.get('pais')
+    comando = '''
+    insert into cad_usuarios (nome,email,sexo,nacionalidade)
+    values('usuario', 'email', 'sexo', 'pais')'''
+    cursor.execute(comando)
+    conexao.commit()
+    print("usuario: {} , email: {} , sexo: {}  , pais: {} ".format(usuario,email,sexo,pais))
+    return "usuario: {} , email: {} , sexo: {} , pais: {} ".format(usuario,email,sexo,pais)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
